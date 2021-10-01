0011ed98de9683327f799765555f9c872a09397d
Uğur Erdal <ugur.erdal@demirorenteknoloji.com>
2021-10-01 21:26:48 +0300

    repo added

---
+++ index.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export function useFetch (){
+  const [loading, setLoading] = useState(false)
+
+  async function fetchData <T> (input: RequestInfo, init?: RequestInit | undefined): Promise<T> {
+    setLoading(true)
+    const request = await fetch(input, init)
+    const result: T = await request.json()
+    setLoading(false)
+    return result
+  }
+  return { 
+    loading,
+    fetch: fetchData
+  }
+}
